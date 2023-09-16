1643a71af3eb1b66ae7db7a7d8dc5c9e881b25ed
YordiPlatzi <shidersax99@gmail.com>
2023-09-15 21:00:45 -0600

    crud vehiculos finalizado

--- Proyecto/src/controllers/VehiculoControler.js
+++ Proyecto/src/controllers/VehiculoControler.js
@@ -20,9 +20,47 @@ controller.save = (req, res) => {
     
     console.log('dato de vehiculo a insertar: ', data)
     req.getConnection((err, conn)=>{
-        conn.query(`insert into vehiculo(Marca_Vehiculo, Tipo_Vehiculo, Modelo_Vehiculo, Placa_Vehiculo, Color_Vehiculo, Kilometraje_Vehiculo, fk_Id_Cliente_V) values ('${data.marca}','${data.tipo}','${data.modelo}','${data.placa}',${data.color}','${data.kilometraje}',${data.cliente})`, (err, vehiculos) => {
+        conn.query(`insert into vehiculo(Marca_Vehiculo, Tipo_Vehiculo, Modelo_Vehiculo, Placa_Vehiculo, Color_Vehiculo, Kilometraje_Vehiculo, fk_Id_Cliente_V) values ('${data.marca}','${data.tipo}','${data.modelo}','${data.placa}','${data.color}','${data.kilometraje}',${data.cliente})`, (err, vehiculos) => { //vehiculos hace referencia al resultado del query
             console.log(vehiculos)
-            res.send('works')
+            res.redirect('/')
+        })
+    })
+}
+
+controller.edit = (req, res) => {
+    const {pk_Id_Vehiculo} = req.params
+
+    req.getConnection((err, conn) =>{
+        conn.query(`select * from vehiculo where pk_Id_Vehiculo = ${pk_Id_Vehiculo}`, (err, vehiculos) =>{
+            res.render("editar_vehiculos" , {
+            data: vehiculos[0] })
+        })
+    })
+}
+
+
+controller.update = (req, res) => {
+
+    const {pk_Id_Vehiculo} = req.params
+    let data = req.body
+    
+    console.log('dato de vehiculo a insertar: ', data)
+    req.getConnection((err, conn)=>{
+        conn.query(`update vehiculo set Marca_Vehiculo = '${data.marca}' , Tipo_Vehiculo = '${data.tipo}' , Modelo_Vehiculo = '${data.modelo}' , Placa_Vehiculo = '${data.placa}' , Color_Vehiculo = '${data.color}' , Kilometraje_Vehiculo = '${data.kilometraje}' , fk_Id_Cliente_V = ${data.cliente} where pk_Id_Vehiculo = ${pk_Id_Vehiculo} `, (err, vehiculos) => { //vehiculos hace referencia al resultado del query
+            console.log(vehiculos)
+            res.redirect('/')
+        })
+    })
+}
+
+
+controller.delete = (req, res) => {
+
+    const {pk_Id_Vehiculo} = req.params
+
+    req.getConnection((err, conn) =>{
+        conn.query(`delete from vehiculo where pk_Id_Vehiculo = ${pk_Id_Vehiculo}`, (err, vehiculos) =>{
+            res.redirect("/")
         })
     })
 }
--- Proyecto/src/routes/VehiculoRoute.js
+++ Proyecto/src/routes/VehiculoRoute.js
@@ -5,5 +5,9 @@ const vehiculoControler = require('../controllers/VehiculoControler')
 
 router.get('/', vehiculoControler.list) //Ruta del API 
 router.post('/add', vehiculoControler.save)
+router.get('/delete/:pk_Id_Vehiculo', vehiculoControler.delete)
+router.get('/update/:pk_Id_Vehiculo', vehiculoControler.edit)
+router.post('/update/:pk_Id_Vehiculo', vehiculoControler.update)
+
 
 module.exports = router;
